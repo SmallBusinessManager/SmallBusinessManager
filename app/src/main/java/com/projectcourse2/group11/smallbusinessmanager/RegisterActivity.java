2bceb90bc34010154c122bfd062d65ada5c8fdc2
DanfengTrondset <danfeng.wang0027@stud.hkr.se>
2017-04-21 21:41:36 +0200

    register page

---
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.projectcourse2.group11.smallbusinessmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
+
