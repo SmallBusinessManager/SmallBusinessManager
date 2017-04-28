324855a509de37b3d40c574a7f6f305adbb7ab90
DanfengTrondset <danfeng.wang0027@stud.hkr.se>
2017-04-28 17:40:29 +0200

    fix register

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/LoginActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/LoginActivity.java
@@ -1,39 +1,13 @@
 package com.projectcourse2.group11.smallbusinessmanager;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -51,24 +25,13 @@ public class LoginActivity extends AppCompatActivity  {
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
 
         mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    return true;
-                }
-                return false;
-            }
-        });
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mEmailView.getText().toString().equals("info@sbm.com") && mPasswordView.getText().toString().equals("123")){
                     Intent intent=new Intent(LoginActivity.this,NavigationActivity.class);
                     startActivity(intent);
-                }
             }
         });
     }
