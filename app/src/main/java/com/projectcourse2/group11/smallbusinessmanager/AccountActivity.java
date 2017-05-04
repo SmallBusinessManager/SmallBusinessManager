f00f986c03a2e2433586a9dda676a7256e351abf
DanfengTrondset <danfeng.wang0027@stud.hkr.se>
2017-05-04 09:20:19 +0200

    account page

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/AccountActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/AccountActivity.java
@@ -1,150 +1,133 @@
 package com.projectcourse2.group11.smallbusinessmanager;
 
-import android.app.DialogFragment;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.projectcourse2.group11.smallbusinessmanager.model.Address;
-import com.projectcourse2.group11.smallbusinessmanager.model.TestPerson;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AccountActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private FirebaseAuth firebaseAuth;
-    private DatabaseReference databaseReference;
-
-    private Button buttonLogout;
-    private Button buttonSave;
-    private Button buttonDeleteAccount;
-    private EditText etFirstName;
-    private EditText etLastName;
-    private EditText etUserName;
-    private EditText etEmail;
-    private EditText etStreet;
-    private EditText etPost;
-    private EditText etCountry;
-    private EditText etPhone;
-    private EditText etSSN;
-    private EditText etCity;
-    private FirebaseUser currentUser;
-    private ProgressDialog progressDialog;
-
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        databaseReference = FirebaseDatabase.getInstance().getReference();
-
-        progressDialog = new ProgressDialog(this);
-
-        if (firebaseAuth.getCurrentUser() == null) {
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
+        LinearLayout ll = (LinearLayout) findViewById(R.id.llMain);
+        for (View view : ll.getTouchables()){
+            if (view instanceof EditText){
+                EditText editText = (EditText) view;
+                editText.setEnabled(false);
+                editText.setFocusable(false);
+                editText.setFocusableInTouchMode(false);
+            }
         }
 
-        currentUser = firebaseAuth.getCurrentUser();
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
-        buttonLogout = (Button) findViewById(R.id.buttonLogout);
-        buttonSave = (Button) findViewById(R.id.buttonSave);
-        buttonDeleteAccount = (Button) findViewById(R.id.buttonDeleteAccount);
-        etFirstName = (EditText) findViewById(R.id.etFirstName);
-        etLastName = (EditText) findViewById(R.id.etLastName);
-        etUserName = (EditText) findViewById(R.id.etUserName);
-        etCountry = (EditText) findViewById(R.id.etCountry);
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        etPost = (EditText) findViewById(R.id.etPost);
-        etPhone = (EditText) findViewById(R.id.etPhone);
-        etStreet = (EditText) findViewById(R.id.etStreet);
-        etSSN = (EditText) findViewById(R.id.etSSN);
-        etCity = (EditText) findViewById(R.id.etCity);
+        prepareListData();
 
-        buttonLogout.setOnClickListener(this);
-        buttonSave.setOnClickListener(this);
-        buttonDeleteAccount.setOnClickListener(this);
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
+        expListView.setAdapter(listAdapter);
+
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                return false;
+            }
+        });
+
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        listDataHeader.add("Personal Information");
+        listDataHeader.add("Account Information");
+        listDataHeader.add("In Company Information");
+
+        List<String> personalInfo = new ArrayList<>();
+        personalInfo.add("SSN:Shawshank Redemption");
+        personalInfo.add("FirstName:Shawshank Redemption");
+        personalInfo.add("LastName:Shawshank Redemption");
+        personalInfo.add("Email:Godfather Part II");
+        personalInfo.add("Phone:Fiction");
+        personalInfo.add("Age:Godfather");
+        personalInfo.add("Gender:Godfather");
+        personalInfo.add("Address:Good the Bad and the Ugly");
+        personalInfo.add("PostCode:Dark Knight");
+        personalInfo.add("City:Angry Men");
+        personalInfo.add("Country:Angry Men");
+
+        List<String> accountInfo = new ArrayList<>();
+        accountInfo.add("UserName:Conjuring");
+        accountInfo.add("Password:cable Me 2");
+
+        List<String> inCompanyInfo = new ArrayList<>();
+        inCompanyInfo.add("Contract ID:2 Gun,s");
+        inCompanyInfo.add("Title:The Sm,urfs 2");
+        inCompanyInfo.add("Salary:Spectacular Now");
+        inCompanyInfo.add("WorkingHour:Cany,ons");
+
+        listDataChild.put(listDataHeader.get(0), personalInfo);
+        listDataChild.put(listDataHeader.get(1), accountInfo);
+        listDataChild.put(listDataHeader.get(2), inCompanyInfo);
     }
 
     @Override
-    public void onClick(View view) {
-        if (view == buttonLogout) {
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
-        }
-        if (view == buttonSave) {
-            saveUserInformation();
-        }
-        if (view == buttonDeleteAccount) {
-            deleteAccount();
-        }
-    }
-
-    private void saveUserInformation() {
-        String ssn = etSSN.getText().toString();
-        String firstName = etFirstName.getText().toString();
-        String lastName = etLastName.getText().toString();
-        String phoneNumber = etPhone.getText().toString();
-        String email = etEmail.getText().toString();
-        Address address = new Address(etStreet.getText().toString(), etCity.getText().toString(), etPost.getText().toString(), etCountry.getText().toString());
-
-        TestPerson testPerson = new TestPerson(ssn, firstName, lastName, phoneNumber, email, address);
-        //todo data validation
-        databaseReference.child(currentUser.getUid()).setValue(testPerson);
-        Toast.makeText(this, "Information saved", Toast.LENGTH_LONG).show();
-    }
-
-    private void deleteAccount() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Delete this account?")
-                .setCancelable(false)
-                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        progressDialog.setMessage("Deleting account");
-                        progressDialog.show();
-                        currentUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    databaseReference.child(currentUser.getUid()).removeValue();
-                                    progressDialog.dismiss();
-                                    AccountActivity.this.finish();
-                                    startActivity(new Intent(AccountActivity.this, LoginActivity.class));
-                                }
-                            }
-                        });
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
+    public void onClick(View v) {
 
     }
-
 }
 
