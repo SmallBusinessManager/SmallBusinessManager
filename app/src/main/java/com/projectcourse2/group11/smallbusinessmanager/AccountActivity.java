7ecbc8ac7c11aac0f41b4658001f9598b0090583
DanfengTrondset <danfeng.wang0027@stud.hkr.se>
2017-05-04 11:04:16 +0200

    account

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/AccountActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/AccountActivity.java
@@ -1,38 +1,82 @@
 package com.projectcourse2.group11.smallbusinessmanager;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class AccountActivity extends AppCompatActivity implements View.OnClickListener {
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
+    private Button buttonEdit;
+    private Button buttonSave;
+    private Button buttonLogout;
+    private Button buttonDeleteAccount;
+
+    private LinearLayout ll;
+    private ProgressDialog progressDialog;
+
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        LinearLayout ll = (LinearLayout) findViewById(R.id.llMain);
-        for (View view : ll.getTouchables()){
-            if (view instanceof EditText){
-                EditText editText = (EditText) view;
-                editText.setEnabled(false);
-                editText.setFocusable(false);
-                editText.setFocusableInTouchMode(false);
-            }
-        }
+        buttonLogout=(Button)findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(this);
 
+        buttonSave=(Button)findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(this);
+
+        buttonDeleteAccount=(Button)findViewById(R.id.buttonDeleteAccount);
+        buttonDeleteAccount.setOnClickListener(this);
+
+        ll = (LinearLayout) findViewById(R.id.llMain);
+        setEditTextTo(false);
+
+        buttonEdit = (Button) findViewById(R.id.buttonEdit);
+        buttonEdit.setOnClickListener(this);
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference();
+        currentUser=firebaseAuth.getCurrentUser();
+        if(currentUser==null){
+            finish();
+            startActivity(new Intent(AccountActivity.this,LoginActivity.class));
+        }
 
         prepareListData();
 
@@ -96,29 +140,41 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
         listDataHeader.add("Personal Information");
         listDataHeader.add("Account Information");
         listDataHeader.add("In Company Information");
-
         List<String> personalInfo = new ArrayList<>();
-        personalInfo.add("SSN:Shawshank Redemption");
-        personalInfo.add("FirstName:Shawshank Redemption");
-        personalInfo.add("LastName:Shawshank Redemption");
-        personalInfo.add("Email:Godfather Part II");
-        personalInfo.add("Phone:Fiction");
-        personalInfo.add("Age:Godfather");
-        personalInfo.add("Gender:Godfather");
-        personalInfo.add("Address:Good the Bad and the Ugly");
-        personalInfo.add("PostCode:Dark Knight");
-        personalInfo.add("City:Angry Men");
-        personalInfo.add("Country:Angry Men");
+        personalInfo.add("SSN:19910115-0000");
+        personalInfo.add("FirstName:Danfeng");
+        personalInfo.add("LastName:Wang");
+        personalInfo.add("Email:danfeng.trondset@gmail.com");
+        personalInfo.add("Phone:0706556305");
+        personalInfo.add("Age:25");
+        personalInfo.add("Gender:FEMALE");
+        personalInfo.add("Address:Fältvägen 3");
+        personalInfo.add("PostCode:12345");
+        personalInfo.add("City:Kristianstad");
+        personalInfo.add("Country:Sweden");
 
         List<String> accountInfo = new ArrayList<>();
-        accountInfo.add("UserName:Conjuring");
-        accountInfo.add("Password:cable Me 2");
+        accountInfo.add("UserName:Danfeng");
+        accountInfo.add("Password:******");
 
         List<String> inCompanyInfo = new ArrayList<>();
-        inCompanyInfo.add("Contract ID:2 Gun,s");
-        inCompanyInfo.add("Title:The Sm,urfs 2");
-        inCompanyInfo.add("Salary:Spectacular Now");
-        inCompanyInfo.add("WorkingHour:Cany,ons");
+        inCompanyInfo.add("Contract ID:sbm45678");
+        inCompanyInfo.add("Title:Employee");
+        inCompanyInfo.add("Salary:0");
+        inCompanyInfo.add("WorkingHour:100.5");
+
+        databaseReference.child(currentUser.getUid()).child("firstName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String firstName=dataSnapshot.getValue(String.class);
+                Log.d("TAG",firstName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         listDataChild.put(listDataHeader.get(0), personalInfo);
         listDataChild.put(listDataHeader.get(1), accountInfo);
@@ -127,7 +183,67 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-
+        if (v==buttonEdit){
+            setEditTextTo(true);
+        }
+        if (v==buttonLogout){
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(AccountActivity.this,LoginActivity.class));
+        }
+        if (v==buttonSave){
+            saveUserInformation();
+        }
+        if (v==buttonDeleteAccount){
+            deleteAccount();
+        }
     }
+
+    private void setEditTextTo(boolean b){
+        for (View view : ll.getTouchables()) {
+            if (view instanceof EditText) {
+                EditText editText = (EditText) view;
+                editText.setEnabled(b);
+                editText.setFocusable(b);
+                editText.setFocusableInTouchMode(b);
+            }
+        }
+    }
+
+    private void saveUserInformation(){
+        //databaseReference.child(currentUser.getUid()).child("firstName").setValue(firstName);
+        Toast.makeText(this, "Information saved", Toast.LENGTH_LONG).show();
+    }
+
+    private void deleteAccount(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Delete this account?")
+                .setCancelable(false)
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        progressDialog.setMessage("Deleting account");
+                        progressDialog.show();
+                        currentUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    databaseReference.child(currentUser.getUid()).removeValue();
+                                    progressDialog.dismiss();
+                                    AccountActivity.this.finish();
+                                    startActivity(new Intent(AccountActivity.this, LoginActivity.class));
+                                }
+                            }
+                        });
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
 }
 
