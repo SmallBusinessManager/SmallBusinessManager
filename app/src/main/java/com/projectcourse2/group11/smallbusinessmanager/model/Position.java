23475c3d5257f64f0101e9d6446e340808b3ed01
Tadas Ivanauskas <ivanauskas15@gmail.com>
2017-05-05 19:42:36 +0200

    Creates new orders to firebase

---
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/model/Position.java
@@ -0,0 +1,9 @@
+package com.projectcourse2.group11.smallbusinessmanager.model;
+
+/**
+ * Created by ivana on 5/5/2017.
+ */
+
+public enum Position {
+    MANAGER, WORKER, ACCOUNTANT, TEAM_LEADER, CUSTOMER
+}
