8346678e275b87f07e9743bf77e2c011f9efda1d
DanfengTrondset <danfeng.wang0027@stud.hkr.se>
2017-05-06 10:35:00 +0200

    project uid

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/ProjectCreatActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/ProjectCreatActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.projectcourse2.group11.smallbusinessmanager.model.Date;
@@ -24,19 +25,21 @@ import com.projectcourse2.group11.smallbusinessmanager.model.Project;
 import com.projectcourse2.group11.smallbusinessmanager.model.TestProject;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ProjectCreatActivity extends AppCompatActivity implements View.OnClickListener{
+public class ProjectCreatActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etProjectName;
     private EditText etProjectDescription;
     private TextView tvStartDate;
     private TextView tvEndDate;
 
-    private int _day,_month,_year;
-    private static final int DIALOG_ID_START=0;
-    private static final int DIALOG_ID_END=1;
+    private int _day, _month, _year;
+    private static final int DIALOG_ID_START = 0;
+    private static final int DIALOG_ID_END = 1;
 
-    private Date startDate,endDate;
+    private Date startDate, endDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,59 +51,59 @@ public class ProjectCreatActivity extends AppCompatActivity implements View.OnCl
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        Calendar calendar=Calendar.getInstance();
-        _year=calendar.get(Calendar.YEAR);
-        _month=calendar.get(Calendar.MONTH);
-        _day=calendar.get(Calendar.DAY_OF_MONTH);
+        Calendar calendar = Calendar.getInstance();
+        _year = calendar.get(Calendar.YEAR);
+        _month = calendar.get(Calendar.MONTH);
+        _day = calendar.get(Calendar.DAY_OF_MONTH);
 
 
-        etProjectName=(EditText)findViewById(R.id.etProjectTitle);
-        etProjectDescription=(EditText)findViewById(R.id.etProjectDescription);
-        tvStartDate=(TextView)findViewById(R.id.tvProjectStartDate);
-        tvEndDate=(TextView)findViewById(R.id.tvProjectEndDate);
+        etProjectName = (EditText) findViewById(R.id.etProjectTitle);
+        etProjectDescription = (EditText) findViewById(R.id.etProjectDescription);
+        tvStartDate = (TextView) findViewById(R.id.tvProjectStartDate);
+        tvEndDate = (TextView) findViewById(R.id.tvProjectEndDate);
         tvStartDate.setOnClickListener(this);
         tvEndDate.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v==tvStartDate){
+        if (v == tvStartDate) {
             showDialog(DIALOG_ID_START);
         }
-        if (v==tvEndDate){
+        if (v == tvEndDate) {
             showDialog(DIALOG_ID_END);
         }
     }
 
     @Override
-    protected Dialog onCreateDialog(int id){
-        if (id==DIALOG_ID_START){
-            return new DatePickerDialog(this,datePickerListener1,_year,_month,_day);
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_ID_START) {
+            return new DatePickerDialog(this, datePickerListener1, _year, _month, _day);
         }
-        if (id==DIALOG_ID_END){
-            return new DatePickerDialog(this,datePickerListener2,_year,_month,_day);
+        if (id == DIALOG_ID_END) {
+            return new DatePickerDialog(this, datePickerListener2, _year, _month, _day);
         }
         return null;
     }
 
-    private DatePickerDialog.OnDateSetListener datePickerListener1=new DatePickerDialog.OnDateSetListener(){
+    private DatePickerDialog.OnDateSetListener datePickerListener1 = new DatePickerDialog.OnDateSetListener() {
         @Override
-        public void onDateSet(DatePicker view,int year,int monthOfYear,int dayOfMonth){
-            _year =year;
-            _month=monthOfYear;
-            _day=dayOfMonth;
-            tvStartDate.setText(_day+"/"+_month+"/"+_year);
-            startDate=new Date(_day,_month,_year);
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            _year = year;
+            _month = monthOfYear;
+            _day = dayOfMonth;
+            tvStartDate.setText(_day + "/" + _month + "/" + _year);
+            startDate = new Date(_day, _month, _year);
         }
     };
-    private DatePickerDialog.OnDateSetListener datePickerListener2=new DatePickerDialog.OnDateSetListener(){
+    private DatePickerDialog.OnDateSetListener datePickerListener2 = new DatePickerDialog.OnDateSetListener() {
         @Override
-        public void onDateSet(DatePicker view,int year,int monthOfYear,int dayOfMonth){
-            _year =year;
-            _month=monthOfYear;
-            _day=dayOfMonth;
-            tvEndDate.setText(_day+"/"+_month+"/"+_year);
-            endDate=new Date(_day,_month,_year);
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            _year = year;
+            _month = monthOfYear;
+            _day = dayOfMonth;
+            tvEndDate.setText(_day + "/" + _month + "/" + _year);
+            endDate = new Date(_day, _month, _year);
         }
     };
 
@@ -112,32 +115,22 @@ public class ProjectCreatActivity extends AppCompatActivity implements View.OnCl
                 //return true;
                 saveToDatebase();
                 finish();
-                startActivity(new Intent(ProjectCreatActivity.this,ProjectActivity.class));
+                startActivity(new Intent(ProjectCreatActivity.this, ProjectActivity.class));
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void saveToDatebase(){
-        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
-        String projectName=etProjectName.getText().toString();
-        String projectDescription=etProjectDescription.getText().toString();
+    private void saveToDatebase() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        String projectName = etProjectName.getText().toString();
+        String projectDescription = etProjectDescription.getText().toString();
 
-        TestProject testProject=new TestProject(projectName,projectDescription,startDate,endDate);
-<<<<<<< HEAD
-        databaseReference.updateChildren(testProject.toHashMap());
-=======
->>>>>>> 32b6a50a5df1609ee2bc7f6c056d028cf290a219
-        //databaseReference.child("project").child("testProject").setValue(testProject);
-       // databaseReference.child("project").child("testProject").child("name").setValue(projectName);
-        //databaseReference.child("project").child("testProject").child("description").setValue(projectDescription);
-        //databaseReference.child("project").child("testProject").child("startDate").setValue(startDate);
-        //databaseReference.child("project").child("testProject").child("endDate").setValue(endDate);
+        TestProject testProject = new TestProject(projectName, projectDescription, startDate, endDate);
+        //get UID and store object under it
+        String key = databaseReference.push().getKey();
+        databaseReference.updateChildren(testProject.toHashMap(key));
 
-        /**
-         * Object can be written like this because it has toHashMap() method.
-         */
-//        databaseReference.updateChildren(testProject.toHashMap());
 
     }
 }
--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/model/TestProject.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/model/TestProject.java
@@ -2,28 +2,30 @@ package com.projectcourse2.group11.smallbusinessmanager.model;
 
 import java.util.HashMap;
 import java.util.Map;
-<<<<<<< HEAD
-import java.util.Random;
 import java.util.UUID;
-=======
->>>>>>> 32b6a50a5df1609ee2bc7f6c056d028cf290a219
 
 public class TestProject {
-    private UUID id;
+    private String id;
     private String name;
     private String description;
     private Date startDate;
     private Date endDate;
 
     public TestProject(String name, String description, Date startDate, Date endDate) {
-        this.id= UUID.randomUUID();
         this.name = name;
         this.description = description;
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
-<<<<<<< HEAD
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -56,26 +58,15 @@ public class TestProject {
         this.endDate = endDate;
     }
 
-    public Map<String,Object> toHashMap(){
-        Map<String,Object> map = new HashMap<>();
-        //map.put("/project/"+id+"/id/",id.toString());
-        map.put("/project/"+id+"/name/",name);
-        map.put("/project/"+id+"/description/",description);
-        map.put("/project/"+id+"/startDate/",startDate.toString());
-        map.put("/project/"+id+"/dueDate/",endDate.toString());
-=======
-    /**
-     * This allow the simple object writing
-     * such as:
-     * databaseReference.updateChildren(testProject);
-     */
-    public Map<String,Object> toHashMap(){
-        Map<String,Object> map = new HashMap<>();
-        map.put("/project/"+name+"/name/",name);
-        map.put("/project/"+name+"/description/",description);
-        map.put("/project/"+name+"/startDate/",startDate);
-        map.put("project/"+name+"/endDate/",endDate);
->>>>>>> 32b6a50a5df1609ee2bc7f6c056d028cf290a219
+    public Map<String, Object> toHashMap(String key) {
+        Map<String, Object> map = new HashMap<>();
+        this.id=key;
+        //map.put("/project/"+id+"/id/",id);
+        map.put("/project/" + id + "/name/", name);
+        map.put("/project/" + id + "/description/", description);
+        map.put("/project/" + id + "/startDate/", startDate.toString());
+        map.put("/project/" + id + "/dueDate/", endDate.toString());
         return map;
     }
 }
+
