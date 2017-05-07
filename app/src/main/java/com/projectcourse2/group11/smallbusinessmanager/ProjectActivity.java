02cf7fedf7d171e72ef0982d2c0754c94708a118
DanfengTrondset <danfeng.wang0027@stud.hkr.se>
2017-05-07 14:03:47 +0200

    taskdb

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/ProjectActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/ProjectActivity.java
@@ -13,6 +13,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.projectcourse2.group11.smallbusinessmanager.model.TestProject;
@@ -41,15 +42,20 @@ public class ProjectActivity extends AppCompatActivity implements View.OnClickLi
         fab.setOnClickListener(this);
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-        DatabaseReference ref= FirebaseDatabase.getInstance().getReference().child("project").child("test");
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference().child("project");
         mAdapter=new FirebaseListAdapter<String>(
                 this,
                 String.class,
                 android.R.layout.simple_list_item_1,
                 ref) {
             @Override
+            protected String parseSnapshot(DataSnapshot snapshot) {
+                return snapshot.child("name").getValue(String.class);
+            }
+            @Override
             protected void populateView(View v, String model, int position) {
                 TextView textView=(TextView)v.findViewById(android.R.id.text1);
+                Log.d("haha",model);
                 textView.setText(model);
             }
         };
