e853ba1dc698dae018d80215c44a4d84f8aa2bd8
Abdulrahman Alassadi <alassadi@live.com>
2017-05-10 15:52:53 +0200

    nothing new

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/AccountantActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/AccountantActivity.java
@@ -21,14 +21,19 @@ public class AccountantActivity extends AppCompatActivity  {
     private Button button;
     private Button button2;
     private ListView listView;
+    private ListView listView2;
     private ListAdapter mAdapter;
+    private ListAdapter mAdapter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountant);
         listView=(ListView)findViewById(R.id.listView);
+        listView2 =(ListView)findViewById(R.id.listView2);
+
         button =(Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
 
         final DatabaseReference ref= FirebaseDatabase.getInstance().getReference().child("companyEmployees").child("company1");
         mAdapter=new FirebaseListAdapter<String>(
@@ -51,6 +56,29 @@ public class AccountantActivity extends AppCompatActivity  {
             public void onClick(View v) {
                 listView.setAdapter(mAdapter);
 
+            }
+        });
+        final DatabaseReference ref2 = FirebaseDatabase.getInstance().getReference().child("companyProjects").child("company1");
+        mAdapter2=new FirebaseListAdapter<String>(
+                this,
+                String.class,
+                android.R.layout.simple_list_item_1,
+                ref) {
+            @Override
+            protected String parseSnapshot(DataSnapshot snapshot) {
+                return snapshot.;
+            }
+            @Override
+            protected void populateView(View v, String model, int position) {
+                TextView textView=(TextView)v.findViewById(android.R.id.text1);
+                textView.setText(model);
+            }
+        };
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                listView2.setAdapter(mAdapter2);
             }
         });
 
