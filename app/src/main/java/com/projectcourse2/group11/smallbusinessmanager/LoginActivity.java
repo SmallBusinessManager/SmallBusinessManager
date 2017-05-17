f6b33f14ac97bdd819fce24e2664fa23d3d91e76
TheSweed <philip.allanson@gmail.com>
2017-05-17 19:15:41 +0200

    Account Activity nearly done. Save Information needs to be fixed and possibly add a bit more account information.

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/LoginActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/LoginActivity.java
@@ -85,7 +85,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         editor = preferences.edit();
         saveLogin = preferences.getBoolean("saveLogin", false);
-        if (saveLogin = true) {
+        if (saveLogin) {
             String name = preferences.getString("username", "");
             String pass = preferences.getString("password", "");
             mEmailView.setText(name);
@@ -137,8 +137,8 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                                                 intent.putExtra("USER",manager);
                                                 break;
                                             case TEAM_LEADER:
-                                                TeamLeader tl = d.getValue(TeamLeader.class);
-                                                intent.putExtra("USER",tl);
+                                                TeamLeader teamLeader = d.getValue(TeamLeader.class);
+                                                intent.putExtra("USER",teamLeader);
                                                 break;
                                             case WORKER:
                                                 Worker worker = d.getValue(Worker.class);
@@ -192,7 +192,6 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                 editor.putString("password", mPasswordView.getText().toString());
                 editor.apply();
             } else {
-                Log.d("Removing all Evidence",loginPreferences.getString("username",""));
                 editor.clear();
                 editor.apply();
             }
