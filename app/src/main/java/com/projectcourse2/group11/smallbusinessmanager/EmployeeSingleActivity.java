9095e1ac75c870cb17af81aefce18f05954225e5
TheSweed <philip.allanson@gmail.com>
2017-05-20 18:22:12 +0200

    Started on EmployeeSingleActivity

--- app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/EmployeeSingleActivity.java
+++ app/src/main/java/com/projectcourse2/group11/smallbusinessmanager/EmployeeSingleActivity.java
@@ -1,9 +1,25 @@
 package com.projectcourse2.group11.smallbusinessmanager;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
 /**
  * Created by Phil on 5/19/2017.
  */
 
-public class EmployeeSingleActivity {
-
+public class EmployeeSingleActivity extends AppCompatActivity {
+    private TextView firstNameText, lastNameText, socialText, emailText, phoneText, positionText;
+    
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_employee_single);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
 }
